3fac30e2ceb40457634069f953cd4149186ce88d
이성재 <adfffewr@naver.com>
2019-12-02 11:06:57 +0900

    회원가입 폼 만들기

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React , {useState} from 'react';
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
+import { Form, Input , Checkbox, Button } from 'antd';
 
 const Signup = () => {
+    const [id , setId] = useState('');
+    const [nick , setNick] = useState('');
+    const [password , setPassword] = useState('');
+    const [passwordCheck , setPasswordCheck] = useState('');
+    const [term , setTerm] = useState(false);
+
+    const onSubmit = () => {
+
+    };
+    const onChangeId = (e) => {
+        setId(e.target.value)
+    };
+    const onChangeNick = (e) => {
+        setNick(e.target.value)
+    };
+    const onChangePassword = (e) => {
+        setPassword(e.target.value)
+    };
+    const onChangePasswordCheck = (e) => {
+        setPasswordCheck(e.target.value)
+    };
+    const onChangeTerm = (e) => {
+        setTerm(e.target.value)
+    };
+    
+
     return(
         <>
         <Head>
@@ -10,7 +37,34 @@ const Signup = () => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.6/antd.css" />
         </Head>
         <AppLayout>
-            <div>회원가입</div>
+            <Form onSubmit={onSubmit} style={{ padding:10 }}>
+                <div>
+                    <label htmlFor="user-id">아이디</label>
+                    <br />
+                    <Input name="user-id" required value={id} onChange={onChangeId} />
+                </div>
+                <div>
+                    <label htmlFor="user-nick">닉네임</label>
+                    <br />
+                    <Input name="user-nick" required  value={nick} onChange={onChangeNick} />
+                </div>
+                <div>
+                    <label htmlFor="user-pass">비밀번호</label>
+                    <br />
+                    <Input name="user-password" type="password" required  value={password} onChange={onChangePassword} />
+                </div>
+                <div>
+                    <label htmlFor="user-pass-check">비밀번호체크</label>
+                    <br />
+                    <Input name="user-password-check" type="password" required value={passwordCheck} onChange={onChangePasswordCheck} />
+                </div>
+                <div>
+                    <Checkbox name="user-term" value={term} onChange={onChangeTerm}>동의해야 합니다.</Checkbox>
+                </div>
+                <div>
+                    <Button type="primary" htmlType="submit">가입하기</Button>
+                </div>
+            </Form>
         </AppLayout>
         </>
     )
