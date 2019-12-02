c4f5b8cda5b240434b06771fc24a480db6067c0f
이성재 <adfffewr@naver.com>
2019-12-02 13:57:48 +0900

    prop-types 적용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 import AppLayout from '../components/AppLayout';
 
 const SNS = ({Component}) => {
@@ -15,5 +16,9 @@ const SNS = ({Component}) => {
         </>
     )
 };
+
+SNS.propTypes = {
+    Component : PropTypes.elementType,
+}
 
 export default SNS;
