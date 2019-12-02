3f0dc1904530ccc4f43f07cb3be35b850bf4815f
이성재 <adfffewr@naver.com>
2019-12-02 13:28:05 +0900

    공통 레이아웃 _app.js 분리

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
-import AppLayout from '../components/AppLayout';
 
 const HOME = () => {
     return(
         <>
-        <Head>
-            <title>SNS</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.6/antd.css" />
-        </Head>
-        <AppLayout>
-            <div>hello11</div>
-        </AppLayout>
+            <div>hello react</div>
         </>
     );
 };
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,6 +1,4 @@
-import React , {useState} from 'react';
-import AppLayout from '../components/AppLayout';
-import Head from 'next/head';
+import React , {useState , useCallback} from 'react';
 import { Form, Input , Checkbox, Button } from 'antd';
 
 const Signup = () => {
@@ -12,7 +10,7 @@ const Signup = () => {
     const [passwordError , setPasswordError] = useState(false);
     const [termError , setTermError] = useState(false);
 
-    const onSubmit = (e) => {
+    const onSubmit = useCallback((e) => {
         e.preventDefault();
         if(password !== passwordCheck) {
             return setPasswordError(true);
@@ -27,7 +25,7 @@ const Signup = () => {
             passwordCheck,
             term
         })
-    };
+    }, [password , passwordCheck , term]);
     const onChangeId = (e) => {
         setId(e.target.value)
     };
@@ -37,54 +35,51 @@ const Signup = () => {
     const onChangePassword = (e) => {
         setPassword(e.target.value)
     };
-    const onChangePasswordCheck = (e) => {
+    const onChangePasswordCheck = useCallback((e) => {
         setPasswordError(e.target.value !== password)
         setPasswordCheck(e.target.value)
-    };
-    const onChangeTerm = (e) => {
+    }, [password]);
+    const onChangeTerm = useCallback((e) => {
         setTermError(false);
         setTerm(e.target.checked)
-    };
+    }, []);
     
 
     return(
         <>
-        <Head>
-            <title>SNS</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.6/antd.css" />
-        </Head>
-        <AppLayout>
-            <Form onSubmit={onSubmit} style={{ padding:10 }}>
-                <div>
-                    <label htmlFor="user-id">아이디</label>
-                    <br />
-                    <Input name="user-id" required value={id} onChange={onChangeId} />
-                </div>
-                <div>
-                    <label htmlFor="user-nick">닉네임</label>
-                    <br />
-                    <Input name="user-nick" required  value={nick} onChange={onChangeNick} />
-                </div>
-                <div>
-                    <label htmlFor="user-pass">비밀번호</label>
-                    <br />
-                    <Input name="user-password" type="password" required  value={password} onChange={onChangePassword} />
-                </div>
-                <div>
-                    <label htmlFor="user-pass-check">비밀번호체크</label>
-                    <br />
-                    <Input name="user-password-check" type="password" required value={passwordCheck} onChange={onChangePasswordCheck} />
-                    {passwordError && <div style={{color:'red'}}>비밀번호가 일치하지 않습니다.</div> }
-                </div>
-                <div>
-                    <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>동의해야 합니다.</Checkbox>
-                    {termError && <div style={{color:'red'}}>약관에 동의하셔야 합니다.</div> }
-                </div>
-                <div style={{marginTop : 10}}>
-                    <Button type="primary" htmlType="submit">가입하기</Button>
-                </div>
-            </Form>
-        </AppLayout>
+        
+        
+        <Form onSubmit={onSubmit} style={{ padding:10 }}>
+            <div>
+                <label htmlFor="user-id">아이디</label>
+                <br />
+                <Input name="user-id" required value={id} onChange={onChangeId} />
+            </div>
+            <div>
+                <label htmlFor="user-nick">닉네임</label>
+                <br />
+                <Input name="user-nick" required  value={nick} onChange={onChangeNick} />
+            </div>
+            <div>
+                <label htmlFor="user-pass">비밀번호</label>
+                <br />
+                <Input name="user-password" type="password" required  value={password} onChange={onChangePassword} />
+            </div>
+            <div>
+                <label htmlFor="user-pass-check">비밀번호체크</label>
+                <br />
+                <Input name="user-password-check" type="password" required value={passwordCheck} onChange={onChangePasswordCheck} />
+                {passwordError && <div style={{color:'red'}}>비밀번호가 일치하지 않습니다.</div> }
+            </div>
+            <div>
+                <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>동의해야 합니다.</Checkbox>
+                {termError && <div style={{color:'red'}}>약관에 동의하셔야 합니다.</div> }
+            </div>
+            <div style={{marginTop : 10}}>
+                <Button type="primary" htmlType="submit">가입하기</Button>
+            </div>
+        </Form>
+
         </>
     )
 };
