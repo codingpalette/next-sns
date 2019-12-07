1aa4b654ea50522adcb11511b871f07168fae5a1
이성재 <iseongjae@iseongjaeui-MacBookAir.local>
2019-12-08 01:21:08 +0900

    댓글 컴포넌트 작업

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,9 +1,16 @@
 import { all , fork , takeLatest , delay , put } from 'redux-saga/effects'
-import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE } from '../reducers/post';
+import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST } from '../reducers/post';
+import axios from 'axios';
 
+function addPostAPI(postData) {
+    return axios.post('/post', postData, {
+        withCredentials: true,
+    });
+};
 
 function* addPost() {
     try{
+        // yield call(addPostAPI); 
         yield delay(2000);
         yield put({
             type : ADD_POST_SUCCESS,
@@ -20,8 +27,35 @@ function* watchAddPost() {
     yield takeLatest(ADD_POST_REQUEST , addPost)
 };
 
+
+function addCommentAPI() {
+
+};
+
+function* addComment(action) {
+    try {
+        yield delay(2000);
+        yield put({
+        type: ADD_COMMENT_SUCCESS,
+        data: {
+            postId: action.data.postId,
+        },
+        });
+    } catch (e) {
+        yield put({
+            type: ADD_COMMENT_FAILURE,
+            error: e,
+        });
+    }
+}
+
+function* watchAddComment() {
+    yield takeLatest(ADD_COMMENT_REQUEST, addComment);
+}
+
 export default function* postSaga() {
     yield all([
         fork(watchAddPost),
+        fork(watchAddComment),
     ]);
 };
