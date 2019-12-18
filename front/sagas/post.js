c187ba31e1e3cd5067406f858c9c3d4b262159b4
이성재 <iseongjae@iseongjaeui-MacBookAir.local>
2019-12-18 21:01:43 +0900

    해시태그 검색

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,5 +1,5 @@
 import { all , fork , takeLatest , delay , put , call } from 'redux-saga/effects'
-import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, LOAD_MAIN_POSTS_SUCCESS, LOAD_MAIN_POSTS_FAILURE, LOAD_MAIN_POSTS_REQUEST } from '../reducers/post';
+import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, ADD_COMMENT_REQUEST, LOAD_MAIN_POSTS_SUCCESS, LOAD_MAIN_POSTS_FAILURE, LOAD_MAIN_POSTS_REQUEST, LOAD_HASHTAG_POSTS_SUCCESS, LOAD_HASHTAG_POSTS_FAILURE, LOAD_HASHTAG_POSTS_REQUEST, LOAD_USER_POSTS_SUCCESS, LOAD_USER_POSTS_FAILURE, LOAD_USER_POSTS_REQUEST } from '../reducers/post';
 import axios from 'axios';
 
 function addPostAPI(postData) {
@@ -52,6 +52,54 @@ function* loadMainPosts() {
 function* watchLoadMainPosts() {
     yield takeLatest(LOAD_MAIN_POSTS_REQUEST, loadMainPosts);
 }
+
+
+function loadHashtagPostsAPI(tag) {
+    return axios.get(`/hashtag/${tag}`);
+}
+
+function* loadHashtagPosts(action) {
+    try {
+        const result = yield call(loadHashtagPostsAPI, action.data);
+        yield put({
+            type: LOAD_HASHTAG_POSTS_SUCCESS,
+            data: result.data,
+        });
+    } catch (e) {
+        yield put({
+            type: LOAD_HASHTAG_POSTS_FAILURE,
+            error: e,
+        });
+    }
+}
+  
+function* watchLoadHashtagPosts() {
+    yield takeLatest(LOAD_HASHTAG_POSTS_REQUEST, loadHashtagPosts);
+};
+
+
+function loadUserPostsAPI(id) {
+    return axios.get(`/user/${id}/posts`);
+}
+  
+function* loadUserPosts(action) {
+    try {
+      const result = yield call(loadUserPostsAPI, action.data);
+      yield put({
+        type: LOAD_USER_POSTS_SUCCESS,
+        data: result.data,
+      });
+    } catch (e) {
+      yield put({
+        type: LOAD_USER_POSTS_FAILURE,
+        error: e,
+      });
+    }
+}
+  
+function* watchLoadUserPosts() {
+    yield takeLatest(LOAD_USER_POSTS_REQUEST, loadUserPosts);
+}
   
 
 
@@ -85,5 +133,7 @@ export default function* postSaga() {
         fork(watchAddPost),
         fork(watchLoadMainPosts),
         fork(watchAddComment),
+        fork(watchLoadHashtagPosts),
+        fork(watchLoadUserPosts),
     ]);
 };
